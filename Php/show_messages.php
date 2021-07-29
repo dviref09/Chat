46e5e46557af1965d41a92974c6738bfcb0d0ef0
Dvir Farkash 2 <dviref09@gmail.com>
2021-07-29 21:47:08 +0300

    commit

--- Php/show_messages.php
+++ Php/show_messages.php
@@ -2,7 +2,8 @@
 
 $conn = mysqli_connect('localhost', 'dvir', 'dvirfarkash09', 'chat');
 
-$result = $conn->query('SELECT * FROM `messages` ORDER BY DESC LIMIT 10');
+$result = $conn->query("SELECT * FROM `messages` ORDER BY `ID` DESC LIMIT 10");
+
 for ($i = 0; $i <= $result->num_rows; $i++) {
     $fetch = $result->fetch_row();
     echo '<div class="message"><h2 class="names">'.$fetch[3].'</h2><h3 class="time">'.$fetch[1].'</h3><p class="message">'.$fetch[2].'</p></div>';
