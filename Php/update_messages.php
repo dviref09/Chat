bcb7f08c62ef0df0c8b1c8345d4a3e1e2f5e4d9e
Dvir Farkash 2 <dviref09@gmail.com>
2021-08-02 20:57:07 +0300

    commit

--- Php/update_messages.php
+++ Php/update_messages.php
@@ -6,7 +6,7 @@ $conn = mysqli_connect('localhost', 'dvir', 'dvirfarkash09', 'chat');
 $result = $conn->query('SELECT * FROM `messages` WHERE `ID` > '.$_SESSION['index'].' ORDER BY `ID` DESC');
 
 if ($result->num_rows > 0){
-        for ($i = 0; $i < $result->num_rows; $i++) {
+    for ($i = 0; $i < $result->num_rows; $i++) {
         $fetch = $result->fetch_row();
         echo '<div class="message"><button name="button_message"></button><h2 class="names">'.$fetch[3].'</h2><h3 class="time">'.$fetch[1].'</h3><p class="message">'.$fetch[2].'</p></div>';
         if ($i == $result->num_rows - 1) {
